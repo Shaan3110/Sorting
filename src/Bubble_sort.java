0de977c05d94a8390626e33163263b31b89238b9
Shaan3110 <hitarasigan0987654321@gmail.com>
2021-02-06 15:47:24 +0530

    Bubble_sort

---
+++ src/Bubble_sort.java
@@ -0,0 +1,30 @@
+
+public class Bubble_sort {
+
+	static void bubble(int arr[],int n)
+	{
+		for(int i=0;i<n-1;i++)
+		{
+			for(int j=0;j<n-1;j++)
+			{
+				if(arr[j]>arr[j+1])
+				{
+					int temp=arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+				}
+			}
+			
+		}
+	}
+
+	public static void main(String[] args) {
+		int arrays[] = {5,6,8,10,1,3,6,9,2,23};
+		bubble(arrays,10);
+		for(int i=0;i<10;i++)
+		{
+			System.out.print(arrays[i]+" ");
+		}
+	}
+
+}
